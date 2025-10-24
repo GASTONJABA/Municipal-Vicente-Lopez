15a7508f7a7007fdf8251d0300c06bba71b07cea
GASTONJABA <GASGHIGO@GMAIL.COM>
2025-10-24 15:49:02 -0300

    con canvas

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -12,9 +12,10 @@ public class PlayerMove : MonoBehaviour
     public Rigidbody Rb;
     private PlayerInput playerInput;
     public InputAction moveAction;
-    public InputAction playerControls;
+    //public InputAction playerControls;
     private Vector2 moveInput;
-    Vector2 movete = Vector2.zero;
+    Vector2 upVector = new Vector2(0f, 1f);
+    //Vector2 movete = Vector2.zero;
     private void Awake()
     {
         Rb = GetComponent<Rigidbody>();
@@ -27,45 +28,47 @@ public class PlayerMove : MonoBehaviour
 
     private void OnEnable()
     {
-        //moveAction.performed += OnMove;
-        playerControls.Enable();
+        moveAction.performed += OnMove;
+        //playerControls.Enable();
     }
 
     private void OnDisable()
     {
-       // moveAction.performed -= OnMove;
-        playerControls.Disable();
+        moveAction.performed -= OnMove;
+       //playerControls.Disable();
     }
 
     // Update is called once per frame
 
     void OnMove(InputAction.CallbackContext context)
     {
-        
-        moveInput = context.ReadValue< Vector2>();
+        //upVector = context.ReadValue<  Vector2>();
+        //moveInput = context.ReadValue< Vector2>();
     }
 
     void Update()
     {
-        movete = playerControls.ReadValue<Vector2>();
-       // HandleMovement();
-      //  transform.position = moveInput;
-       // Vector2 move = transform.up * moveInput.y + transform.forward * moveInput.y;
+        moveInput = moveAction.ReadValue<Vector2>();
+        //movete = playerControls.ReadValue<Vector2>();
+        HandleMovement();
+       //transform.position = moveInput;
+       // transform.position = upVector;
+        // Vector2 move = transform.up * moveInput.y + transform.forward * moveInput.y;
     }
 
 
     void HandleMovement()
     {
-        transform.position = moveInput;
-        Vector2 move = transform.up * moveInput.y + transform.forward * moveInput.y;
-        Vector2 velocity = new Vector2(move.x * moveSpeed, Rb.velocity.y);
+       //transform.position = moveInput;
+        Vector2 move = transform.up * moveInput.y + transform.forward * moveInput.x;
+        Vector3 velocity = new Vector3(move.x * moveSpeed, Rb.velocity.y);
         Rb.velocity = velocity;
     }
 
 
     private void FixedUpdate()
     {
-        Rb.velocity = new Vector2 (movete.y *  moveSpeed, movete.x * moveSpeed);
+        //Rb.velocity = new Vector2 (move *  moveSpeed, move.x * moveSpeed);
     }
 
 }
